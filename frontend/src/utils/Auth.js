ad3128d69b381a4613422b645fdf154771f7e5e4
frestosnn <fresto.n@yandex.ru>
2023-11-15 22:25:31 +0300

    тест

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,6 +1,7 @@
-export const BASE_URL = 'https://api.frestosnn.students.nomoredomainsmonster.ru';
+export const BASE_URL =
+  //'https://api.frestosnn.students.nomoredomainsmonster.ru';
 
-//'http://localhost:3000';
+  'http://localhost:3000';
 
 //'https://api.frestosnn.students.nomoredomainsmonster.ru';
 
