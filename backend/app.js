53c23f158feccd50549a477ab73d156254b81014
frestosnn <fresto.n@yandex.ru>
2023-11-15 21:31:02 +0300

    4

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,22 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const helmet = require("helmet");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
 
-const { celebrate, Joi } = require("celebrate");
-const { errors } = require("celebrate");
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 
-const { createUser, login } = require("./controllers/users");
+const { createUser, login } = require('./controllers/users');
 
-const userRouter = require("./routes/users");
-const cardRouter = require("./routes/cards");
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
-const errorHandler = require("./middlewares/error-handler");
-const PathError = require("./errors/path-errors");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-const auth = require("./middlewares/auth");
+const errorHandler = require('./middlewares/error-handler');
+const PathError = require('./errors/path-errors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
-  process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(cors());
@@ -26,32 +24,32 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.disable("x-powered-by");
+app.disable('x-powered-by');
 
 // через localhost не получается подключиться, первая ссылка с решением проблемы со StackOverflow
 mongoose.connect(DB_URL);
 
 // краш тест
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Сервер сейчас упадёт");
+    throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
 
 app.use(requestLogger);
 app.post(
-  "/signin",
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().email().required(),
       password: Joi.string().min(8).required(),
     }),
   }),
-  login
+  login,
 );
 
 app.post(
-  "/signup",
+  '/signup',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
@@ -59,20 +57,20 @@ app.post(
       avatar: Joi.string()
         .uri()
         .pattern(
-          /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.\w+))(:\d{2,5})?((\/.+)+)?\/?#?/
+          /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.\w+))(:\d{2,5})?((\/.+)+)?\/?#?/,
         ),
       email: Joi.string().email().required(),
       password: Joi.string().min(8).required(),
     }),
   }),
-  createUser
+  createUser,
 );
 
-app.use("/users", userRouter);
-app.use("/cards", cardRouter);
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 app.use((req, res, next) => {
-  const err = new PathError("Not Found: Маршрут не найден");
+  const err = new PathError('Not Found: Маршрут не найден');
   next(err);
 });
 
